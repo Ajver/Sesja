6377014253c2aeba46ed644719d4c6b64cf6c705
Dominik <40212297+Ajver@users.noreply.github.com>
2018-06-25 00:02:10 +0200

    Some so nice changes
    Lot of css changes (added arrow, changed question boxes, etc.)

--- situations.js
+++ situations.js
@@ -23,7 +23,7 @@ function createSituations() {
 	
 	// Creating situation 2
 	situations.push(new Situation(
-	"To jest trzecia sytuacja (id=2), bdawdwajbd wabdh awudh wadb hbdwahb dwabhb wadbh bwadhb dhbawh bwahdb hwadbh wadhb hwadb bdawhbd awhbh bdwahbdwahbdawh bh awbhdbawh bhdawb hdb awhdbwahb dw. dwajdjaw dj w adwjad wand wandn wandw adnn awnwd audaw uhduh wadj awdnuaw udybn awyd byjdo ud awy dawjn aw.",
+	"To jest trzecia sytuacja (id=2), bdawdwajbd wadwbdh awudffh wadb hbdwahb dwabhb wadbh bwadhb dhbawh bwahdb hwadbh wadhb hwadb bdawhbd awhbh bdwahbdwahbdawh bh awbhdbawh bhdawb hdb awhdbwahb dw. dwajdjaw dj w adwjad wand wandn wandw adnn awnwd audaw uhduh wadj awdnuaw udybn awyd byjdo ud awy dawjn aw.",
 	new Array(
 	new Question("GoTO 1", 0, 1),
 	new Question("Gotooo 0", 1, 0)
